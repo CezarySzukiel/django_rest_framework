ee35cc67e1af0fd82826b69aee67f5c2ee51c5c1
Cezary <cezary.szukiel@gmail.com>
2023-12-17 23:03:34 +0100

    admin

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'subtitle', 'author', 'isbn')
+    search_fields = ('title', 'subtitle', 'author', 'isbn')
+
